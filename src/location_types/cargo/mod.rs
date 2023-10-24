ed5eff6e272f4f1d1f7670ab201411cce79f514f
Tom Oram <me@tomoram.io>
2023-10-24 18:21:52 +0100

    style: Fix lint issues

--- src/location_types/cargo/mod.rs
+++ src/location_types/cargo/mod.rs
@@ -17,11 +17,13 @@ fn check_cargo_is_installed() -> anyhow::Result<()> {
         .spawn()
         .map_err(|err| anyhow!("Failed to execute cargo, please make sure it is installed: {err}"))
         .and_then(wait_for_command)
-        .and_then(returned_zero_or_error(|result| anyhow!(
-            "Failed to get cargo version (exit code: {}): {}",
-            result.exit_status,
-            result.stderr
-        )))
+        .and_then(returned_zero_or_error(|result| {
+            anyhow!(
+                "Failed to get cargo version (exit code: {}): {}",
+                result.exit_status,
+                result.stderr
+            )
+        }))
         .map(|_| ())
 }
 
@@ -34,11 +36,13 @@ fn install_cargo_edit() -> anyhow::Result<()> {
         .spawn()
         .map_err(|err| anyhow!("Failed install cargo-edit: {err}"))
         .and_then(wait_for_command)
-        .and_then(returned_zero_or_error(|result| anyhow!(
-            "Failed to install cargo-edit (exit code: {}): {}",
-            result.exit_status,
-            result.stderr
-        )))
+        .and_then(returned_zero_or_error(|result| {
+            anyhow!(
+                "Failed to install cargo-edit (exit code: {}): {}",
+                result.exit_status,
+                result.stderr
+            )
+        }))
         .map(|_| ())
 }
 
@@ -51,39 +55,42 @@ fn update_version(version: &str) -> anyhow::Result<()> {
         .spawn()
         .map_err(|err| anyhow!("Failed to update Cargo version: {err}"))
         .and_then(wait_for_command)
-        .and_then(returned_zero_or_error(|result| anyhow!(
-            "Failed to set version (exit code: {}): {}",
-            result.exit_status,
-            result.stderr
-        )))
+        .and_then(returned_zero_or_error(|result| {
+            anyhow!(
+                "Failed to set version (exit code: {}): {}",
+                result.exit_status,
+                result.stderr
+            )
+        }))
         .map(|_| ())
 }
 
 struct CommandResult {
     exit_status: ExitStatus,
-    stdout: String,
     stderr: String,
 }
 
 fn wait_for_command(mut child: Child) -> anyhow::Result<CommandResult> {
     let exit_status = child.wait()?;
 
-    let mut stdout = String::new();
-    child.stdout.as_mut().ok_or(anyhow!("failed to unwrap stdout"))?.read_to_string(&mut stdout)?;
-
     let mut stderr = String::new();
-    child.stderr.as_mut().ok_or(anyhow!("failed to unwrap stderr"))?.read_to_string(&mut stderr)?;
+    child
+        .stderr
+        .as_mut()
+        .ok_or(anyhow!("failed to unwrap stderr"))?
+        .read_to_string(&mut stderr)?;
 
-    return Ok(CommandResult {
-        stdout,
-        stderr,
+    Ok(CommandResult {
         exit_status,
-    });
+        stderr,
+    })
 }
 
-fn returned_zero_or_error<E>(create_error: E) -> impl Fn(CommandResult) -> anyhow::Result<CommandResult>
+fn returned_zero_or_error<E>(
+    create_error: E,
+) -> impl Fn(CommandResult) -> anyhow::Result<CommandResult>
 where
-    E: Fn(CommandResult) -> anyhow::Error
+    E: Fn(CommandResult) -> anyhow::Error,
 {
     move |result| {
         if result.exit_status.success() {
