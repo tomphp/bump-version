5ac9e7b910387fc7e559b0f35f6855c5552cf14b
Tom Oram <me@tomoram.io>
2023-10-26 22:08:25 +0100

    refactor: Extract Command

--- src/main.rs
+++ src/main.rs
@@ -19,21 +19,12 @@
     clippy::panic_in_result_fn
 )]
 
-use std::pin::Pin;
+use clap::Parser;
 
-use app_state::AppState;
-use async_stream::stream;
-use clap::{Parser, Subcommand};
-use futures::{Stream, StreamExt};
-
-use config::Location;
-use formatter::plain::Plain;
-use update_event::UpdateEvent;
-use Location::{Cargo, StringPattern};
-
-use crate::config::Config;
+use command::Command;
 
 mod app_state;
+mod command;
 mod config;
 mod formatter;
 mod location_types;
@@ -43,95 +34,10 @@ mod update_event;
 #[command(author, version, about, long_about = None)]
 struct Cli {
     #[command(subcommand)]
-    command: Commands,
-}
-
-#[derive(Subcommand)]
-enum Commands {
-    /// Updates the version in all known locations
-    Update {
-        /// The new value to be used when representing a value
-        #[arg()]
-        version: String,
-    },
+    command: Command,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
-    let cli = Cli::parse();
-
-    match &cli.command {
-        Commands::Update { version } => update_command(version).await?,
-    };
-
-    Ok(())
-}
-
-async fn update_command(version: &str) -> Result<(), anyhow::Error> {
-    const CONFIG_FILE: &str = "versioned-files.yml";
-
-    let config = Config::from_file(CONFIG_FILE);
-
-    let mut streams = config.map(|config| config.locations).map(
-        |locations| -> Pin<Box<dyn Stream<Item = UpdateEvent> + Send>> {
-            Box::pin(futures::stream::select_all(locations.into_iter().map(
-                |location| update_location(1, version.to_string(), location),
-            )))
-        },
-    )?;
-
-    let mut formatter = Plain {};
-    let mut state = AppState::new(&mut formatter);
-
-    while let Some(event) = streams.next().await {
-        state.update_event(&event);
-    }
-
-    state.as_result()
-}
-
-fn update_location(
-    id: usize,
-    version: String,
-    location: Location,
-) -> Pin<Box<dyn Stream<Item = UpdateEvent> + Send>> {
-    match location {
-        StringPattern(location_config) => {
-            Box::pin(update_string_pattern_location(id, version, location_config))
-        }
-        Cargo => Box::pin(update_cargo_location(id, version)),
-    }
-}
-
-fn update_cargo_location(id: usize, version: String) -> impl Stream<Item = UpdateEvent> {
-    stream! {
-        yield UpdateEvent::Started(id, "Cargo.toml".to_string());
-        match location_types::cargo::update_cargo_version(&version) {
-            Ok(()) => {
-              yield UpdateEvent::Succeeded(id);
-              yield UpdateEvent::Started(id, "Cargo.lock".to_string());
-              yield UpdateEvent::Succeeded(id);
-            }
-            Err(err) => {
-                yield UpdateEvent::Failed(id, err.to_string());
-            }
-        }
-    }
-}
-
-fn update_string_pattern_location(
-    id: usize,
-    version: String,
-    location_config: location_types::string_pattern::Config,
-) -> impl Stream<Item = UpdateEvent> {
-    stream! {
-        yield UpdateEvent::Started(id, location_config.file.clone());
-        match location_types::string_pattern::replace_version(
-            &location_config,
-            &version,
-        ) {
-            Ok(()) => yield UpdateEvent::Succeeded(id),
-            Err(err) => yield UpdateEvent::Failed(id, err.to_string()),
-        }
-    }
+    Cli::parse().command.execute().await
 }
